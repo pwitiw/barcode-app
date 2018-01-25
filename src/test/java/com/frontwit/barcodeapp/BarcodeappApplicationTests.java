2191c07d98b9ec747a291e530177997356615c5b
patryk <pwitiw93@gmail.com>
2018-01-25 22:41:58 +0100

    init commit

---
+++ src/test/java/com/frontwit/barcodeapp/BarcodeappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.frontwit.barcodeapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BarcodeappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
