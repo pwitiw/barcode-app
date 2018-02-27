035093c78dadaa859d6f8f985477e50ec0d3559b
Patryk <pwitiw93@gmail.com>
2018-02-27 18:30:13 +0100

    database config

--- src/main/java/com/frontwit/barcodeapp/BeanConfig.java
+++ src/main/java/com/frontwit/barcodeapp/BeanConfig.java
@@ -1,9 +1,28 @@
 package com.frontwit.barcodeapp;
 
+import com.frontwit.barcodeapp.entity.Order;
+import com.frontwit.barcodeapp.repository.OrderRepository;
+import com.github.mongobee.Mongobee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import javax.annotation.PostConstruct;
 
 @Configuration
 public class BeanConfig {
 
 
+    @Autowired
+    OrderRepository orderRepository;
+
+    @PostConstruct
+    public void test() {
+        Order order = Order.builder().id(1L).name("TestName").build();
+        Order order2 = Order.builder().id(2L).name("TestName2").build();
+        orderRepository.save(order);
+        orderRepository.save(order2);
+    }
 }
---
+++ src/main/java/com/frontwit/barcodeapp/MongoConfig.java
@@ -0,0 +1,55 @@
+package com.frontwit.barcodeapp;
+
+import com.github.mongobee.Mongobee;
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+
+@Configuration
+public class MongoConfig {
+
+    private final static String CHANGELOG_PACKAGE = "com.frontwit.barcodeapp";
+    @Value("${spring.data.mongodb.database}")
+    private String dbName;
+
+    public @Bean
+    MongoDbFactory mongoDbFactory() {
+        return new SimpleMongoDbFactory(new MongoClient(), dbName);
+    }
+
+    public @Bean
+    MongoTemplate mongoTemplate() throws Exception {
+        //remove _class
+        MappingMongoConverter converter =
+                new MappingMongoConverter(new DefaultDbRefResolver(mongoDbFactory()), new MongoMappingContext());
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+        MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory(), converter);
+        return mongoTemplate;
+    }
+
+    @Bean
+    @Autowired
+    public Mongobee mongobee(Environment environment) {
+        String prefix = "mongodb://";
+        String host = environment.getProperty("spring.data.mongodb.host");
+        String port = environment.getProperty("spring.data.mongodb.port");
+
+        String uri = prefix + host + ":" + port;
+        Mongobee runner = new Mongobee(uri);
+        runner.setDbName(dbName);
+        runner.setChangeLogsScanPackage(CHANGELOG_PACKAGE);
+        return runner;
+    }
+
+}
