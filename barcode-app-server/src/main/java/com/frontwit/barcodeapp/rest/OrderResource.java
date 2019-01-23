63dacc4e75d6cee95a297de4eb1de918aece09ba
Spigiel <michal.spigiel@capgemini.com>
2019-01-23 13:47:20 +0100

    i

--- barcode-app-server/src/main/java/com/frontwit/barcodeapp/rest/OrderResource.java
+++ barcode-app-server/src/main/java/com/frontwit/barcodeapp/rest/OrderResource.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/orders")
 public class OrderResource {
