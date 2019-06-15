a6a424a8b644fccdc98921834f203f86dd949867
Patryk Witiw <witiw.patryk@swm.de>
2019-06-15 12:19:21 +0200

    added rest service, proxy

--- barcode-app-client/src/app/services/rest.service.ts
+++ barcode-app-client/src/app/services/rest.service.ts
@@ -1,28 +1,28 @@
 import {HttpClient, HttpResponse} from "@angular/common/http";
-import {catchError, tap} from "rxjs/operators";
+import {catchError} from "rxjs/operators";
 import {Observable, of} from "rxjs";
 import {Injectable} from "@angular/core";
 
 @Injectable()
 export class RestService {
 
-  private static readonly BASE_URL = "/api";
-
   constructor(private http: HttpClient) {
   }
 
-  public post(url: string, body: any): Observable<HttpResponse<Object>> {
-    // @ts-ignore
-    return this.http.post(RestService.BASE_URL + url, body, {observe: "response"}).pipe(
-      tap(_ => console.log('Authorization in progress')),
-      catchError(this.handleError<string>())
-    );
+  public post<T>(url: string, body: any): Observable<HttpResponse<T>> {
+    return this.http.post(url, body, {observe: "response"}).pipe(
+      catchError(this.handleError)
+    ) as Observable<HttpResponse<T>>;
   }
 
-  private handleError<T>(result?: T) {
-    return (error: any): Observable<T> => {
-      console.log(error.message);
-      return of(result as T);
-    };
+  public get<T>(url): Observable<T> {
+    return this.http.get(url).pipe(
+      catchError(this.handleError)
+    ) as Observable<T>;
   }
+
+  private handleError<T>(error: any): Observable<T> {
+    console.error("Rest call error: " + error.message);
+    return of(error);
+  };
 }
