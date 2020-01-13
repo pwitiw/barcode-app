078c65a07aab1aa698d25bd95b4aab65b5f6aec9
patryk <pwitiw93@gmail.com>
2020-01-13 22:07:48 +0100

    modified logging path, added log in sync

--- administration/src/main/java/com/frontwit/barcodeapp/administration/processing/synchronization/OrderSynchronizer.java
+++ administration/src/main/java/com/frontwit/barcodeapp/administration/processing/synchronization/OrderSynchronizer.java
@@ -1,10 +1,13 @@
 package com.frontwit.barcodeapp.administration.processing.synchronization;
 
+import com.frontwit.barcodeapp.administration.processing.front.application.ProcessingFront;
 import com.frontwit.barcodeapp.administration.processing.front.model.FrontNotFound;
 import com.frontwit.barcodeapp.administration.processing.shared.OrderId;
 import com.frontwit.barcodeapp.administration.processing.shared.ProcessingException;
 import com.frontwit.barcodeapp.administration.processing.shared.events.DomainEvents;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
 
 import java.time.LocalDate;
@@ -13,6 +16,7 @@ import static java.lang.String.format;
 
 @AllArgsConstructor
 public class OrderSynchronizer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderSynchronizer.class);
 
     private SourceOrderRepository sourceOrderRepository;
     private SaveSynchronizedFronts saveSynchronizedFronts;
@@ -48,5 +52,6 @@ public class OrderSynchronizer {
         var targetOrder = orderMapper.map(sourceOrder, dictionary);
         saveSynchronizedOrder.save(targetOrder);
         saveSynchronizedFronts.save(targetOrder.getFronts());
+        LOGGER.debug(format("Synchronized order (id=%s) with %s fronts", targetOrder.getOrderId().getOrderId(), targetOrder.getFronts().size()));
     }
 }
--- administration/src/main/java/com/frontwit/barcodeapp/administration/processing/synchronization/SourceOrderRepository.java
+++ administration/src/main/java/com/frontwit/barcodeapp/administration/processing/synchronization/SourceOrderRepository.java
@@ -3,7 +3,6 @@ package com.frontwit.barcodeapp.administration.processing.synchronization;
 import com.frontwit.barcodeapp.administration.processing.shared.OrderId;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
