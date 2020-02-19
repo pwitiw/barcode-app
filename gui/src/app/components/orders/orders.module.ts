985771f209a5506c1d7a36cd29c732f83983049d
patryk <pwitiw93@gmail.com>
2020-02-19 17:29:34 +0100

    dependency versions moved to properties

--- gui/src/app/components/orders/orders.module.ts
+++ gui/src/app/components/orders/orders.module.ts
@@ -21,7 +21,6 @@ import {
 import {StageService} from "./stage.service";
 import {FormsModule} from "@angular/forms";
 import {MAT_MOMENT_DATE_ADAPTER_OPTIONS, MatMomentDateModule} from "@angular/material-moment-adapter";
-import {MatNativeDateModule} from "@angular/material/core";
 
 @NgModule({
     declarations: [
