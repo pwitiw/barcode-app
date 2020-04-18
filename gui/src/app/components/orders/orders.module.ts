a4e2e1bc9a3cf023dfb665e18ac840d33a43a1fe
patryk <pwitiw93@gmail.com>
2020-04-18 13:29:48 +0200

    changed order view

--- gui/src/app/components/orders/orders.module.ts
+++ gui/src/app/components/orders/orders.module.ts
@@ -2,7 +2,6 @@ import {NgModule} from "@angular/core";
 import {OrderRow} from './order-row/order-row.component';
 import {OrdersComponent} from './orders.component';
 import {OrderRestService} from './order.rest.service';
-import {OrderDetailComponent} from './order-detail/order-detail.component';
 import {SearchCriteriaComponent} from './search-criteria/search-criteria.component';
 import {CommonModule} from "@angular/common";
 import {CommonsModule} from "../commons/commons.module";
@@ -29,7 +28,6 @@ import {MatDialogModule} from "@angular/material/dialog";
     declarations: [
         OrderRow,
         OrdersComponent,
-        OrderDetailComponent,
         SearchCriteriaComponent,
         OrderDetailsDialog
     ],
