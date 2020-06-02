82ef197eefe5edba9f841062b52b8a650928d2e8
Justyna Pokrzywinska <j.pokrzywinska@gmail.com>
2020-06-02 21:20:28 +0200

    refactoring after review

--- administration/src/main/java/com/frontwit/barcodeapp/administration/processing/order/model/OrderType.java
+++ administration/src/main/java/com/frontwit/barcodeapp/administration/processing/order/model/OrderType.java
@@ -1,8 +1,5 @@
 package com.frontwit.barcodeapp.administration.processing.order.model;
 
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
 public enum OrderType {
     COMPLAINT,
     ORDER
--- administration/src/main/java/com/frontwit/barcodeapp/administration/processing/synchronization/OrderTypeMapper.java
+++ administration/src/main/java/com/frontwit/barcodeapp/administration/processing/synchronization/OrderTypeMapper.java
@@ -4,11 +4,11 @@ import com.frontwit.barcodeapp.administration.processing.order.model.OrderType;
 
 public class OrderTypeMapper {
 
+    private static final String COMPLAINT = "r";
+
     public static OrderType map(String value) {
-        if ("r".equals(value))
+        if (COMPLAINT.equals(value)) {
             return OrderType.COMPLAINT;
-        else if ("z".equals(value)) {
-            return OrderType.ORDER;
         }
         return OrderType.ORDER;
     }
