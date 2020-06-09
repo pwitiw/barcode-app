11f086a743713981f1896edd279b0b1a8be7c479
patryk <pwitiw93@gmail.com>
2020-06-09 17:50:35 +0200

    customer integrity service off

--- administration/src/main/java/com/frontwit/barcodeapp/administration/processing/synchronization/CustomerIntegrityService.java
+++ administration/src/main/java/com/frontwit/barcodeapp/administration/processing/synchronization/CustomerIntegrityService.java
@@ -13,27 +13,34 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.HashMap;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @AllArgsConstructor
-@Service
 public class CustomerIntegrityService {
     private static final Logger LOGGER = LoggerFactory.getLogger(Front.class);
 
     private final MongoTemplate mongoTemplate;
     private final CustomerSynchronizer customerSynchronizer;
 
-    @PostConstruct
     void assureIntegrity() {
         customerSynchronizer.synchronize();
-        var customers = mongoTemplate.findAll(CustomerEntity.class).stream()
-                .collect(Collectors.toMap(CustomerEntity::getName, Function.identity()));
-        List<OrderEntity> orders = mongoTemplate.find(Query.query(Criteria.where("customerId").is(null)), OrderEntity.class);
+        var customers = new HashMap<String, CustomerEntity>();
+        mongoTemplate.findAll(CustomerEntity.class)
+                .forEach(c -> customers.putIfAbsent(c.getName(), c));
+        LOGGER.info("Customer amount {}", customers.size());
+
+        List<OrderEntity> orders = mongoTemplate.findAll(OrderEntity.class);
         orders.forEach(o -> {
-            o.setCustomerId(customers.get(o.getCustomer()).getId());
-            mongoTemplate.save(o);
+            var customer = customers.get(o.getCustomer());
+            if (customer != null) {
+                o.setCustomerId(customer.getId());
+                mongoTemplate.save(o);
+            } else {
+                LOGGER.warn("No customer found for name {}", o.getCustomer());
+            }
         });
         LOGGER.info("Modified {} records", orders.size());
     }
