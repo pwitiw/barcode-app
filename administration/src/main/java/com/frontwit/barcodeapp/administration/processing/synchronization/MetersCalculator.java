c67fedbced9e6d201247f5b9df6e7026e218a66f
patryk <pwitiw93@gmail.com>
2020-06-21 22:11:13 +0200

    meters calculator

--- administration/src/main/java/com/frontwit/barcodeapp/administration/processing/synchronization/MetersCalculator.java
+++ administration/src/main/java/com/frontwit/barcodeapp/administration/processing/synchronization/MetersCalculator.java
@@ -1,4 +1,15 @@
 package com.frontwit.barcodeapp.administration.processing.synchronization;
 
+import com.frontwit.barcodeapp.administration.statistics.domain.order.Meters;
+
+import java.util.List;
+
 public class MetersCalculator {
+    public static Meters calculate(List<TargetFront> fronts) {
+        var result = fronts.stream()
+                .map(front -> front.getDimensions().getHeight() * front.getDimensions().getWidth() * front.getQuantity().getValue())
+                .reduce(0, Integer::sum)
+                .doubleValue();
+        return Meters.of(result);
+    }
 }
