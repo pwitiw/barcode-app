051fd43a57819d3b37f45584e154e469aec9698c
pwitiw <pwitiw93@gmail.com>
2020-12-19 08:34:54 +0100

    cleanup after release

--- administration/src/main/java/com/frontwit/barcodeapp/administration/processing/order/application/CompleteOrderScheduler.java
+++ administration/src/main/java/com/frontwit/barcodeapp/administration/processing/order/application/CompleteOrderScheduler.java
@@ -12,6 +12,7 @@ import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
