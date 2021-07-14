e9875b5a04fa2706f88363559e6aedad46a049c0
Justyna Witiw <j.pokrzywinska@gmail.com>
2021-07-14 21:18:56 +0200

    backend - delete tests data

--- administration/src/main/java/com/frontwit/barcodeapp/administration/statistics/application/StatisticsEventsHandler.java
+++ administration/src/main/java/com/frontwit/barcodeapp/administration/statistics/application/StatisticsEventsHandler.java
@@ -4,9 +4,6 @@ import com.frontwit.barcodeapp.administration.processing.front.model.Front;
 import com.frontwit.barcodeapp.administration.processing.front.model.FrontProcessed;
 import com.frontwit.barcodeapp.administration.processing.front.model.FrontRepository;
 import com.frontwit.barcodeapp.administration.processing.front.model.FrontStatisticsRepository;
-import com.frontwit.barcodeapp.administration.processing.shared.Barcode;
-import com.frontwit.barcodeapp.administration.processing.shared.OrderId;
-import com.frontwit.barcodeapp.administration.processing.shared.Stage;
 import com.frontwit.barcodeapp.administration.statistics.domain.OrderPlaced;
 import com.frontwit.barcodeapp.administration.statistics.domain.order.OrderStatistics;
 import com.frontwit.barcodeapp.administration.statistics.domain.order.OrderStatisticsRepository;
@@ -15,10 +12,8 @@ import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Optional;
 
@@ -50,15 +45,5 @@ public class StatisticsEventsHandler {
         var date = event.getDateTime().atZone(ZoneId.of("Europe/Warsaw"));
         StatisticsPeriod period = StatisticsPeriod.hourlyFrom(date);
         stageStatisticsCalculator.saveStageStatistics(period, event.getStage(), front.get());
-    }
-
-    @Scheduled(initialDelay = 1_000, fixedDelay = Long.MAX_VALUE)
-    void generateStatistics() {
-        //todo wykresy
-        handle(new FrontProcessed(Barcode.valueOf(new OrderId(1L), 1), Stage.PACKING, LocalDateTime.now()));
-        handle(new FrontProcessed(Barcode.valueOf(new OrderId(1L), 1), Stage.PACKING, LocalDateTime.now()));
-        handle(new FrontProcessed(Barcode.valueOf(new OrderId(1L), 1), Stage.PACKING, LocalDateTime.of(2021, 7, 13, 13, 59)));
-        handle(new FrontProcessed(Barcode.valueOf(new OrderId(1L), 1), Stage.PACKING, LocalDateTime.of(2021, 7, 13, 14, 00)));
-        handle(new FrontProcessed(Barcode.valueOf(new OrderId(1L), 1), Stage.PACKING, LocalDateTime.of(2021, 7, 13, 14, 00)));
     }
 }
